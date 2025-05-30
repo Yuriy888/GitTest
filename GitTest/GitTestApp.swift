9b770aae2e937cc811d5fa163c4f6fd021f26ad7
Yurii <daus.yuriy@gmail.com>
2025-05-30 12:31:03 +0300

    Initial Commit

---
+++ GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by Yurii Daus on 30.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
